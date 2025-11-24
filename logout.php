bbc14985d546934ea6c7ff76e3052007e56573db
Echo Hex <134620215+arghadey@users.noreply.github.com>
2025-11-24 16:29:30 +0600

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();       // remove all session variables
-session_destroy();     // destroy the session
-setcookie(session_name(), '', time() - 3600, '/'); // clear session cookie
-
-header('Location: login.php');
-exit;
-?>
